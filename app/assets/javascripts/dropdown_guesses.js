e8da03066651fbe995e40a1bea82891846981c96
iandenty <denty29@gmail.com>
2014-12-10 12:03:18 +0000

    dynamic markers

--- app/assets/javascripts/dropdown_guesses.js
+++ app/assets/javascripts/dropdown_guesses.js
@@ -41,8 +41,6 @@ function guess_dropdown(){
   $("#guess_common_name").on("change", function(){
     $("#guess_button").parent().show();
   });
-
-
   $("form#new_guess").submit(function(){
     var guessInfo = $(this).serialize(0);
     $.ajax({
@@ -66,45 +64,3 @@ function guess_dropdown(){
     return false;
   });
 }
-
-function guess_next_page(){
-  $("#next_practice_image").on("click", function(){
-    get_new_image("practice"); 
-  });
-};
-
-function identify_next_page(){
-  $("#next_identify_image").on("click", function(){
-    get_new_image("identify"); 
-  });
-};
-
-function set_map_size(){
-  var mapHeight = $('#update_image').height();
-  var mapWidth = $('#update_image').width();
-  map_toggle(mapHeight, mapWidth);
-  $('#magnify_tab').click(magnify_image);
-};
-
-function get_new_image(route){
-  $('#photo_frame').empty();
-  $.get("/images/"+route+"/", function(data){
-    $('#photo_frame').append(data);
-    $('#update_image').on("load", set_map_size);
-    if(route==="practice"){
-      guess_dropdown();
-      guess_next_page();
-    } else if(route==="identify"){
-      identify_dropdown();
-      identify_next_page();
-    };
-  });
-};
-
-function magnify_image(){
-  $('#update_image').loupe({
-    width: 200,
-    height: 150,
-    loupe: 'loupe'
-  });
-};
--- app/assets/javascripts/map_toggle.js
+++ app/assets/javascripts/map_toggle.js
@@ -1,7 +1,5 @@
 function map_toggle(mapHeight, mapWidth){
   if($('#map').length) {
-    // var mapHeight = $('#update_image').height();
-    // var mapWidth = $('#update_image').width();
     $('#map').css({'height':mapHeight+'px'});
     $('#map').css({'width':mapWidth+'px'});
     var map = L.map('map').setView([54.001282, -3.333363], 4);
@@ -22,21 +20,27 @@ function map_toggle(mapHeight, mapWidth){
       $('#update_image').toggle();
       $('#map').toggle();
     });
-    map.on('click', function(e) { 
-      if($('.leaflet-marker-icon').length){
-        $('.leaflet-marker-icon, .leaflet-marker-shadow, .leaflet-zoom-animated').remove()
-        var lat = e.latlng.lat;
-        var lng = e.latlng.lng;
-        var marker = L.marker([lat, lng]).addTo(map);
-        $(image_lat).val(lat)
-        $(image_long).val(lng)
-      } else {
-        var lat = e.latlng.lat;
-        var lng = e.latlng.lng;
-        var marker = L.marker([lat, lng]).addTo(map);
-        $(image_lat).val(lat)
-        $(image_long).val(lng)
-      }
-    });
+    if($("#image_lat").length && $("#image_long").length){
+      marker_on_click(map);
+    }
   };
 };
+
+function marker_on_click(map){
+  map.on('click', function(e) { 
+    if($('.leaflet-marker-icon').length){
+      $('.leaflet-marker-icon, .leaflet-marker-shadow, .leaflet-zoom-animated').remove()
+      var lat = e.latlng.lat;
+      var lng = e.latlng.lng;
+      var marker = L.marker([lat, lng]).addTo(map);
+      $("#image_lat").val(lat)
+      $("#image_long").val(lng)
+    } else {
+      var lat = e.latlng.lat;
+      var lng = e.latlng.lng;
+      var marker = L.marker([lat, lng]).addTo(map);
+      $("#image_lat").val(lat)
+      $("#image_long").val(lng)
+    }
+  });
+}
