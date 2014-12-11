7061279214773c10605b7cd84ae109019257aec4
iandenty <denty29@gmail.com>
2014-12-11 12:33:33 +0000

    seed instructions added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# To seed images
+# Image.create image: File.open(File.join(Rails.root, "db", "pictures_seeds", "image1.png"))
