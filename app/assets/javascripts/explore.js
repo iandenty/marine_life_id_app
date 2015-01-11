aa4fc8a3be6c7ef3b25d2a45415ebb4786cc8440
iandenty <denty29@gmail.com>
2015-01-11 16:02:27 +0000

    js explore deleted

--- app/assets/javascripts/explore.js
+++ app/assets/javascripts/explore.js
@@ -1,8 +1,2 @@
-function append_explore_map(){
-    var map = L.map('map').setView([54.001282, -3.333363], 4);
-    var ggleLayer;
-    ggleLayer = new L.Google('TERRAIN')
-    map.addLayer(ggleLayer);
-    map.invalidateSize();
-  }
+
 
